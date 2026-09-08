ce075254a3fbdfaac118f482c9ac2fb43ae758ab
John-Paul Takats <jptkts@gmail.com>
2026-09-08 12:14:21 -0400

    adding our end of day 3A material

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 //JP's App for day 3A
 
-//Test anohter change
+//Test anohter
 
 import 'package:flutter/material.dart';
 
@@ -14,7 +14,44 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Week3App(),
     );
+  }
+}
+
+class Week3App extends StatefulWidget {
+  const Week3App({super.key});
+
+  @override
+  State<Week3App> createState() => _Week3AppState();
+}
+
+class _Week3AppState extends State<Week3App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.yellow,
+          title: Text("Week 3A App"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width:200,
+              height:100,
+              color:Colors.green,
+              child: Text("Hello Week 1")
+            ),
+            
+            Container(
+              width:150,
+              height:100,
+              color:Colors.yellow,
+              child: Text("Hello Week 2")              
+            )
+          ],
+        ), 
+        );
   }
 }
